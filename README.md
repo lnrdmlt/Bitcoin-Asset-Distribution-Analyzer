6988b0381d680df00f7065d68e1ef7dc65b52e50
lnrdmlt <leomalott@gmail.com>
2023-03-02 16:32:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bitcoin Wealth Distribution!
+Analyzing the asset distribution of Bitcoin owners by value.  
+
+# Usage
+Generate a CSV file from the Bitcoin chain state, in the following format: 
+	
+```
+address,value_satoshi,last_height
+1M5h7XBjKNk6yETdpqav9rhdgAFmsUDMGb,73349,389950
+```
+Reference this code if you need help extract the data from Bitcoin chain into a CSV file.
+https://github.com/graymauser/btcposbal2csv
+
+Rename CSV file into `data/addresses.csv`
---
+++ sorted_value_by_percent.py
@@ -0,0 +1,47 @@
+import csv
+
+def sort():
+    with open("addresses.csv", mode="r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        return sorted(csv_reader, key=lambda row:int(row["value_satoshi"]) , reverse=True)   
+    
+def output(data):
+    line_count = 0
+    for row in data:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            print(
+            f'\tThis address({row["address"]}) has {row["value_satoshi"]} satoshi, at block {row["last_height"]}.'
+            )
+            line_count += 1
+            print(f"Processed {line_count} lines.")
+
+def get_total_num_wallets(sorted_data):
+    total = 0
+    for row in sorted_data:
+        total += 1
+    return total
+
+def get_num_wallets_by_percent(sorted_data, percent):
+    total = get_total_num_wallets(sorted_data)
+    return total * percent /100
+
+def get_total_value_wallets_by_percent(sorted_data, start_percent, end_percent):
+    total = 0
+    count = 0
+    start_count = get_num_wallets_by_percent(sorted_data, start_percent)
+    end_count = get_num_wallets_by_percent(sorted_data, end_percent)
+    for row in sorted_data:
+        count += 1
+        if count <= start_count:
+            continue
+        if count > end_count:
+            break
+        total += int(row["value_satoshi"])
+    return total
+            
+       
+sorted_data = sort()   
+print(get_total_value_wallets_by_percent(sorted_data,0,1))
+print(get_total_value_wallets_by_percent(sorted_data,1,10))
